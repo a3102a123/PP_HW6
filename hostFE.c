976ff5aede305925c5accaed60073efe24961c06
DavidTu <a3102a123@gamil.com>
2020-12-29 15:57:02 +0800

    add opencl host & kernel

--- hostFE.c
+++ hostFE.c
@@ -7,7 +7,34 @@ void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
             float *inputImage, float *outputImage, cl_device_id *device,
             cl_context *context, cl_program *program)
 {
-    cl_int status;
+    cl_int status,ret;
+    int img_size = imageWidth * imageHeight;
     int filterSize = filterWidth * filterWidth;
-
+    int half = filterWidth / 2;
+    // create command queue
+    cl_command_queue command_queue = clCreateCommandQueue(*context, *device, 0, &ret);
+    // create kernel mem
+    cl_mem input_img_mem = clCreateBuffer(*context, CL_MEM_READ_ONLY, img_size * sizeof(float), NULL, &ret);
+    cl_mem filter_mem = clCreateBuffer(*context, CL_MEM_READ_ONLY, filterSize * sizeof(float), NULL, &ret);
+    cl_mem width = clCreateBuffer(*context, CL_MEM_READ_ONLY, sizeof(int), NULL, &ret);
+    cl_mem height = clCreateBuffer(*context, CL_MEM_READ_ONLY, sizeof(int), NULL, &ret);
+    cl_mem halffilterSize = clCreateBuffer(*context, CL_MEM_READ_ONLY, sizeof(int), NULL, &ret);
+    cl_mem output_img_mem = clCreateBuffer(*context, CL_MEM_WRITE_ONLY, img_size * sizeof(float), NULL, &ret);
+    // init mem
+    ret = clEnqueueWriteBuffer(command_queue, input_img_mem, CL_TRUE, img_size * sizeof(float), inputImage, 0, NULL, NULL);
+    ret = clEnqueueWriteBuffer(command_queue, filter_mem, CL_TRUE, filterSize * sizeof(float), filter, 0, NULL, NULL);
+    ret = clEnqueueWriteBuffer(command_queue, width, CL_TRUE, sizeof(int), &imageWidth, 0, NULL, NULL);
+    ret = clEnqueueWriteBuffer(command_queue, height, CL_TRUE, sizeof(int), &imageHeight, 0, NULL, NULL);
+    ret = clEnqueueWriteBuffer(command_queue, halffilterSize, CL_TRUE, sizeof(int), &half, 0, NULL, NULL);
+    // create kernel
+    cl_kernel kernel = clCreateKernel(program, "convolution", &ret);
+    // set kernel arg
+    ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&input_img_mem);
+    ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&filter_mem);
+    ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&width);
+    ret = clSetKernelArg(kernel, 3, sizeof(cl_mem), (void *)&height);
+    ret = clSetKernelArg(kernel, 4, sizeof(cl_mem), (void *)&halffilterSize);
+    ret = clSetKernelArg(kernel, 5, sizeof(cl_mem), (void *)&output_img_mem);
+    // run kernel
+    ret = clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, &global_item_size, &local_item_size, 0, NULL, NULL);
 }
--- kernel.cl
+++ kernel.cl
@@ -1,4 +1,17 @@
-__kernel void convolution() 
+__kernel void convolution(__global const float *input_img, __global const float *filter,  __global int imageWidth, __global int imageHeight, __global int halffilterSize, __global float *output_img) 
 {
-   
+    int i = get_global_id(0) % imageWidth;
+    int j = get_global_id(0) / imageWidth;
+    float sum;
+    for (l = -halffilterSize; l <= halffilterSize; l++)
+    {
+        if (i + k >= 0 && i + k < imageHeight &&
+            j + l >= 0 && j + l < imageWidth)
+        {
+            sum += input_img[(i + k) * imageWidth + j + l] *
+                    filter[(k + halffilterSize) * filterWidth +
+                            l + halffilterSize];
+        }
+    }
+    output_img[i * imageWidth + j] = sum;
 }
